6b23661b5b581e4c8677f396998d461dcfd38be5
ONDREJF <ONDREJF@amdocs.com>
2026-01-15 23:05:59 +0100

    init

---
+++ src/main/java/com/of/ll/LiveLiveBackendApplication.java
@@ -0,0 +1,13 @@
+package com.of.ll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveLiveBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiveLiveBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/of/ll/LiveLiveBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.of.ll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiveLiveBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
